253c754cedcb3f971152beb603f8edffa7c5c260
Matthew Mihok <4d.69.68.6f.6b@gmail.com>
2019-04-02 05:06:54 -0400

    Adding neuron and initial network code

---
+++ helpers/Math.js
@@ -0,0 +1,4 @@
+// Our sigmoid function that takes in an input
+window.Math.sigmoid = function ( x ) {
+  return 1 / (1 + Math.exp((-x)));
+}
---
+++ helpers/math.lib.js
@@ -0,0 +1,12 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],[
+/* 0 */
+/***/ (function(module, exports) {
+
+// Our sigmoid function that takes in an input
+window.Math.sigmoid = function ( x ) {
+  return 1 / (1 + Math.exp((-x)));
+}
+
+
+/***/ })
+],[[0,0]]]);
